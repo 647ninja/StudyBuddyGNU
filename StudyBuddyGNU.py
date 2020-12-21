5984a1a09ebeaefcf7127603ea07009e9a065552
647ninja <647ninja@pm.me>
2020-12-21 15:06:40 -0500

    update

--- StudyBuddyGNU.py
+++ StudyBuddyGNU.py
@@ -1,17 +1,28 @@
-# Study Buddy For Linux
-# R.
-# 2020-12-19
-# Requirements
-# https://gitea.com/chopin42/countdown > pip install countdown-timer
+# Program: Study Buddy For Linux
+# Author: R.
+# Original creation date: 2020-12-19
+# Version: 2
+# Updated: 2020-12-21
 
+#!/usr/bin/env python3
 import random
-from countdown import countdown
+import time
+import sys
+
+def countdown(mins, secs=0):
+    t = (mins*60) + secs
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
 
 a = []
 with open("topics.txt", "r") as f:
   a = f.readlines()
 print("Let's study, how about?:",random.choice(a)),
 input("Press Enter to Start timer")
-countdown(30),
+countdown(1),
 import subprocess as s
-s.call(['notify-send','Finished!, Well done, have a Good Day!'])
+s.call(['notify-send','Well done!, Have a Great Day!']),print('\a')
